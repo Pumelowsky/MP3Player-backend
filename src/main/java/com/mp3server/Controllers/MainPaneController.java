470e90f18d68cb843d39e2e0aafd71cfe672a217
Patryk Wojewoda <wojewodix@gmail.com>
2024-06-06 21:18:11 +0200

    Dodanie obsługi odtwarzania muzyki

--- src/main/java/com/mp3server/Controllers/MainPaneController.java
+++ src/main/java/com/mp3server/Controllers/MainPaneController.java
@@ -1,11 +1,29 @@
 package com.mp3server.Controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mp3server.server.Song;
 import javafx.animation.ScaleTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.util.Duration;
+import javazoom.jl.player.Player;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class MainPaneController {
 
@@ -25,14 +43,59 @@ public class MainPaneController {
     private Button nextSongButton;
 
     @FXML
-    private ComboBox<Double> speedBox;
+    private ComboBox<String> speedBox;
+
+    @FXML
+    private Slider volumeSlider;
 
 
     @FXML
     private Label songLabel;
 
     @FXML
+    private TableView<Song> songTable;
+
+    @FXML
+    private TableColumn<Song, String> songNameColumn;
+
+    @FXML
+    private TableColumn<Song, String> songDurationColumn;
+
+    @FXML
+    private TableColumn<Song, String> songArtistColumn;
+    @FXML
+    private Label currentSongLabel;
+    @FXML
+    private ProgressBar songProgresBar;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    private File directory;
+    private File[] files;
+    private ArrayList<File> songs;
+    private int songNumber;
+    private int[] speeds = {25,50,75,100,125,150,175,200};
+    private Timer timer;
+    private TimerTask task;
+    private boolean running;
+
+
+
+    //private List<Song> songs;
+    private Thread playerThread;
+    private Player player = null;
+    private File currentSong = null;
+    private int currentSongIndex;
+    private static final String BASE_URL = "http://localhost:8080";
+    private boolean paused = true;
+    private final Object pauseLock = new Object();
+
+    @FXML
     public void initialize() {
+        // Ścieżka do folderu
+        String folderPath = "src/main/java/com/mp3server/music";
+
         addScaleTransition(playButton);
         addScaleTransition(pauseButton);
         addScaleTransition(resetSongButton);
@@ -40,8 +103,49 @@ public class MainPaneController {
         addScaleTransition(nextSongButton);
 
         addPulsingEffect(songLabel);
-    }
+        //loadSongs();
 
+        songs = new ArrayList<File>();
+
+        directory = new File(folderPath);
+
+        files = directory.listFiles();
+
+        if(files != null) {
+
+            for(File file : files) {
+
+                songs.add(file);
+            }
+        }else{
+            System.err.println("Nie znaleziono nic w folderze music");
+        }
+
+
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        songLabel.setText(songs.get(songNumber).getName());
+
+        for(int i = 0; i < speeds.length; i++) {
+
+            speedBox.getItems().add(Integer.toString(speeds[i])+"%");
+        }
+
+        speedBox.setOnAction(this::changeSpeed);
+
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+
+                //mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+            }
+        });
+
+        songProgresBar.setStyle("-fx-accent: #00FF00;");
+
+    }
     private void addScaleTransition(Button button) {
         button.setOnMousePressed(event -> {
             ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100), button);
@@ -69,17 +173,17 @@ public class MainPaneController {
 
     @FXML
     private void playSong() {
-
+        mediaPlayer.play();
     }
 
     @FXML
     private void pauseSong() {
-
+        mediaPlayer.pause();
     }
 
     @FXML
     private void resetSong() {
-
+        mediaPlayer.seek(Duration.seconds(0));
     }
 
     @FXML
@@ -89,11 +193,58 @@ public class MainPaneController {
 
     @FXML
     private void playNextSong() {
+        if(songNumber<songs.size()-1){
+            songNumber++;
+            mediaPlayer.stop();
+        }
+    }
+
+    @FXML
+    private void changeSpeed(ActionEvent event) {
+
+    }
+
+    public void beginTimmer(){
+
+    }
+    public void cancelTimmer(){
 
     }
 
     @FXML
-    private void changeSpeed() {
+    private List<Song> getAllSongs() throws IOException {
+        URL url = new URL(BASE_URL + "/allsongs");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
 
+        int responseCode = conn.getResponseCode();
+
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+            Scanner scanner = new Scanner(url.openStream());
+            StringBuilder inline = new StringBuilder();
+            while (scanner.hasNext()) {
+                inline.append(scanner.nextLine());
+            }
+            scanner.close();
+
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(inline.toString(), new TypeReference<List<Song>>() {});
+        }
     }
+
+//    @FXML
+//    private void loadSongs() {
+//        try {
+//            songs = getAllSongs();
+//            ObservableList<Song> songList = FXCollections.observableArrayList(songs);
+//            songTable.setItems(songList);
+//            currentSongLabel.setText(songs.get(0).getTytul());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module server {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires javafx.media;
     requires spring.core;
     requires spring.beans;
     requires spring.context;
@@ -12,10 +13,12 @@ module server {
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.web;
-
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires jlayer;
 
     exports com.mp3server;
     exports com.mp3server.server;
-    opens com.mp3server.Controllers to javafx.fxml;
-    opens com.mp3server.server to spring.core, spring.beans, spring.context;
+    opens com.mp3server.Controllers to javafx.fxml, com.fasterxml.jackson.databind;
+    opens com.mp3server.server to spring.core, spring.beans, spring.context, com.fasterxml.jackson.databind;
 }
